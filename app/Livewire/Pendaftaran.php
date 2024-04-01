e96a19fe43684f2a16ef8a0eaf7c458354a5bb5e
ferdiansahgg <ferdiansah1605@gmail.com>
2024-04-01 12:56:59 +0700

    halaman pendaftaran

--- app/Livewire/Pendaftaran.php
+++ app/Livewire/Pendaftaran.php
@@ -6,6 +6,9 @@ use Livewire\Component;
 
 class Pendaftaran extends Component
 {
+    public $nama = 'Ferdiansah';
+    public $email = 'Ferdiansah@gmail.com';
+    public $alamat = 'Jalan bangka 2 no 3';
     public function render()
     {
         return view('livewire.pendaftaran');
--- resources/views/livewire/pendaftaran.blade.php
+++ resources/views/livewire/pendaftaran.blade.php
@@ -1,3 +1,63 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    <div class="container">
+        <!-- START FORM -->
+        <div class="my-3 p-3 bg-body rounded shadow-sm">
+            <form>
+                <div class="mb-3 row">
+                    <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" value="{{ $nama }}">
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="email" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" value="{{ $email }}">
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" value="{{ $alamat }}">
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label class="col-sm-2 col-form-label"></label>
+                    <div class="col-sm-10"><button type="button" class="btn btn-primary" name="submit" wire:click="store()">SIMPAN</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <!-- AKHIR FORM -->
+
+        <!-- START DATA -->
+        <h1>Data Penerima Beasiswa</h1>
+        <div class="my-3 p-3 bg-body rounded shadow-sm">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th class="col-md-1">No</th>
+                        <th class="col-md-4">Nama</th>
+                        <th class="col-md-3">Email</th>
+                        <th class="col-md-2">Alamat</th>
+                        <th class="col-md-2">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Muhammad</td>
+                        <td>muhammad@gmail.com</td>
+                        <td>Yogyakarta</td>
+                        <td>
+                            <a href="" class="btn btn-warning btn-sm">Edit</a>
+                            <a href="" class="btn btn-danger btn-sm">Del</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+        <!-- AKHIR DATA -->
+    </div>
 </div>

c5772e0404506ade711452dd41d66e055c6d52eb
ferdiansahgg <ferdiansah1605@gmail.com>
2024-04-01 13:32:52 +0700

    update koneksi database dan simpan data

--- app/Livewire/Pendaftaran.php
+++ app/Livewire/Pendaftaran.php
@@ -2,13 +2,31 @@
 
 namespace App\Livewire;
 
+use App\Models\Pendaftaran as ModelsPendaftaran;
 use Livewire\Component;
 
 class Pendaftaran extends Component
 {
-    public $nama = 'Ferdiansah';
-    public $email = 'Ferdiansah@gmail.com';
-    public $alamat = 'Jalan bangka 2 no 3';
+    public $nama;
+    public $email;
+    public $alamat;
+    public function store()
+    {
+        $rules = [
+            'nama' => 'required',
+            'email' => 'required|email',
+            'alamat' => 'required',
+        ];
+        $pesan = [
+            'nama.required' => 'Nama tidak boleh kosong',
+            'email.required' => 'Email tidak boleh kosong',
+            'email.email' => 'Email tidak valid',
+            'alamat.required' => 'Alamat tidak boleh kosong',
+        ];
+        $validatedData = $this->validate($rules, $pesan);
+        ModelsPendaftaran::create($validatedData);
+        session()->flash('message', 'Data berhasil disimpan');
+    }
     public function render()
     {
         return view('livewire.pendaftaran');
--- resources/views/livewire/pendaftaran.blade.php
+++ resources/views/livewire/pendaftaran.blade.php
@@ -1,29 +1,46 @@
 <div>
     <div class="container">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            
+        @endif
+
+        @if (session()->has('message'))
+            <div class="alert alert-success">
+                {{ session('message') }}
+            </div>
+            
+        @endif
         <!-- START FORM -->
         <div class="my-3 p-3 bg-body rounded shadow-sm">
             <form>
                 <div class="mb-3 row">
                     <label for="nama" class="col-sm-2 col-form-label">Nama</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" value="{{ $nama }}">
+                        <input type="text" class="form-control" wire:model='nama'>
                     </div>
                 </div>
                 <div class="mb-3 row">
                     <label for="email" class="col-sm-2 col-form-label">Email</label>
                     <div class="col-sm-10">
-                        <input type="email" class="form-control" value="{{ $email }}">
+                        <input type="email" class="form-control" wire:model='email'>
                     </div>
                 </div>
                 <div class="mb-3 row">
                     <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" value="{{ $alamat }}">
+                        <input type="text" class="form-control" wire:model='alamat'>
                     </div>
                 </div>
                 <div class="mb-3 row">
                     <label class="col-sm-2 col-form-label"></label>
-                    <div class="col-sm-10"><button type="button" class="btn btn-primary" name="submit" wire:click="store()">SIMPAN</button>
+                    <div class="col-sm-10"><button type="button" class="btn btn-primary" name="submit" wire:click="store">SIMPAN</button>
                     </div>
                 </div>
             </form>
